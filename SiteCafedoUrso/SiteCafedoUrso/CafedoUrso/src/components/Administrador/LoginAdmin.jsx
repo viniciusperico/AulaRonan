6870c7714dac5c906771368ba9a7e544dc65c2cc
Vinicius Périco <105397291+viniciusperico@users.noreply.github.com>
2024-08-24 23:55:21 -0300

    update

--- SiteCafedoUrso/SiteCafedoUrso/CafedoUrso/src/components/Administrador/LoginAdmin.jsx
+++ SiteCafedoUrso/SiteCafedoUrso/CafedoUrso/src/components/Administrador/LoginAdmin.jsx
@@ -1,58 +1,59 @@
 // FormCadastro.js
-import React, { useState } from 'react';
-import { db } from '../../database/firebaseConfig';
-import { collection, addDoc } from 'firebase/firestore';
+import React, { useState } from "react";
+import { db } from "../../database/firebaseConfig";
+import { collection, addDoc } from "firebase/firestore";
 import "./LoginAdmin.css";
 
 const FormCadastro = () => {
-  const [titulo, setTitulo] = useState('');
-  const [descricao, setDescricao] = useState('');
-  const [imagem, setImagem] = useState('');
+  const [titulo, setTitulo] = useState("");
+  const [descricao, setDescricao] = useState("");
+  const [imagem, setImagem] = useState("");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     try {
-      await addDoc(collection(db, 'items'), {
+      await addDoc(collection(db, "items"), {
         titulo,
         descricao,
-        imagem
+        imagem,
       });
-      setTitulo('');
-      setDescricao('');
-      setImagem('');
+      setTitulo("");
+      setDescricao("");
+      setImagem("");
     } catch (err) {
       console.error("Erro ao adicionar documento: ", err);
     }
   };
 
   return (
-    
-    <form className='incluir-cards' onSubmit={handleSubmit}>
-      <h3>INCLUSÃO DE CARDS NA PAGINA INICIAL</h3>
-      <input 
-        type="text" 
-        placeholder="Título" 
-        value={titulo} 
-        onChange={(e) => setTitulo(e.target.value)} 
-        required 
-      />
-      <input 
-        type="text" 
-        placeholder="Descrição" 
-        value={descricao} 
-        onChange={(e) => setDescricao(e.target.value)} 
-        required 
-      />
-      <input 
-        type="text" 
-        placeholder="URL da Imagem" 
-        value={imagem} 
-        onChange={(e) => setImagem(e.target.value)} 
-        required 
-      />
-      <button type="submit">Adicionar</button>
-    </form>
+    <body class="pag-incluir-cards">
+      <form className="incluir-cards" onSubmit={handleSubmit}>
+        <h3>INCLUSÃO DE CARDS NA PAGINA INICIAL</h3>
+        <input
+          type="text"
+          placeholder="Título"
+          value={titulo}
+          onChange={(e) => setTitulo(e.target.value)}
+          required
+        />
+        <input
+          type="text"
+          placeholder="Descrição"
+          value={descricao}
+          onChange={(e) => setDescricao(e.target.value)}
+          required
+        />
+        <input
+          type="text"
+          placeholder="URL da Imagem"
+          value={imagem}
+          onChange={(e) => setImagem(e.target.value)}
+          required
+        />
+        <button type="submit">Adicionar Card</button>
+      </form>
+    </body>
   );
 };
 
