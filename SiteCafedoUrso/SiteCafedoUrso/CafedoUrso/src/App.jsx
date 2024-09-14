0ec29d7b681fcb135ea4a1c6eb6cff16b3c2242d
Vinicius Périco <105397291+viniciusperico@users.noreply.github.com>
2024-09-14 10:34:04 -0300

    Update App.jsx

--- SiteCafedoUrso/SiteCafedoUrso/CafedoUrso/src/App.jsx
+++ SiteCafedoUrso/SiteCafedoUrso/CafedoUrso/src/App.jsx
@@ -14,7 +14,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
+    <Router>+
       <Routes>
         <Route
           path="/login"
